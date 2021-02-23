8d5f41467d61233400116b490982fd4f2a657d57
yiyiay <yigit.dinc@ug.bilkent.edu.tr>
2021-02-23 16:17:49 +0300

    Update StudentSearch.java

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
 
 public class StudentSearch {
-    public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
-        if (students.isEmpty()) {
-            throw new Exception("Students list should not be empty!");
-        }
-        for (Student student: students)
-            if (student.getId().equals(id))
-                return true;
-
-        return false;
-    }
+    	public boolean studentExists(ArrayList<Student> students, String id) throws Exception {
+       if (students.isEmpty()) {
+           throw new Exception("Students list should not be empty!");
+       }
+       for (Student student: students)
+       if (student.getId().equals(id))
+         return true;
+       return false;
+      }
 
    public Student findOne(ArrayList<Student> students, String name) throws Exception {
       for (Student student: students)
